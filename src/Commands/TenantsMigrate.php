0ec3a929990c143974d82702f2439e96cca820e7
Jordi Puigdellívol <jordi.p@revo.works>
2018-01-09 13:38:28 +0100

    closing connections when migrating

--- src/Commands/TenantsMigrate.php
+++ src/Commands/TenantsMigrate.php
@@ -8,41 +8,52 @@ use DB;
 use Artisan;
 use Exception;
 
-class TenantsMigrate extends BaseTenantsCommand {
-
+class TenantsMigrate extends BaseTenantsCommand
+{
     protected $signature = 'tenants:migrate
                                 {--rollback : Rollback all tenants or tenant selected}
                                 {--tenant= : Specify a tenant to migrate (or rollback)}';
 
     protected $description = 'Migrate all tenants';
 
-    protected function preHandle(){
-        $this->info('');    
+    protected function preHandle()
+    {
+        $this->info('');
         $this->info(' ===== TENANTS MIGRATION =====');
         $this->info('');
 
-        if( $this->option('rollback') ){
+        if ($this->option('rollback')) {
             $this->error('*******************************');
             $this->error('**       ROLLING BACK        **');
             $this->error('*******************************');
             $this->info('');
         }
-         return $this->confirm('Do you wish to continue?',false);
+        return $this->confirm('Do you wish to continue?', false);
     }
 
-    protected function postHandle(){ 
+    protected function postHandle()
+    {
         $this->info('Dump autoload');
         exec('composer dump-autoload');
         Artisan::call('optimize');
         Artisan::call('queue:restart'); //To reload the code for queues (on daemon)
     }
 
-    protected function handleTenant($tenant){
+    protected function handleTenant($tenant)
+    {
         $class = config('tenants.user');
         createDBConnection($tenant);
-        if( $this->option('rollback') ){  
+        if ($this->option('rollback')) {
             return $class::rollback($tenant);
         }
         $class::migrate($tenant);
+        $this->closeAllConnections();   //To avoid the `too many connections exception`
+    }
+
+    private function closeAllConnections()
+    {
+        foreach (app('db')->getConnections() as $connection) {
+            $connection->disconnect();
+        }
     }
 }
